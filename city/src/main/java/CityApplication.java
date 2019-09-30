0efdfc98c79f1135bc8f16b4a54e2bb9e4f722fe
ljinfeng_glb <ljinfeng_glb@gmail.com>
2019-09-30 11:59:20 +0800

    create project springcloud-demo

---
+++ city/src/main/java/CityApplication.java
@@ -0,0 +1,10 @@
+package src.main.java;
+
+/**
+ * @Author :ljinfeng@linewell.com
+ * @Date :2019-09-30
+ */
+
+
+public class CityApplication {
+}
---
+++ demo-eureka/src/main/java/EurekaApplication.java
@@ -0,0 +1,18 @@
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * 注册中心
+ * @Author :ljinfeng@linewell.com
+ * @Date :2019-09-30
+ */
+@EnableEurekaServer
+@SpringBootApplication
+public class EurekaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaApplication.class, args);
+    }
+
+}
